d6cf2b4128c770017368fa4b718abe56ea15edcc
tmogoa <mogoa.tonny@gmail.com>
2022-08-15 13:05:23 +0300

    Added gradients

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="w-full h-screen flex flex-col justify-center items-center bg-blue-600 gap-y-5">
+        <div className="w-full h-screen flex flex-col justify-center items-center gap-y-5 bg-gradient-to-r from-cyan-400 to-blue-800">
             <div className="text-white text-3xl">I am</div>
             <div className="font-bold text-6xl text-blue-500 font-heading bg-white p-6 rounded">
                 Tony Mogoa
