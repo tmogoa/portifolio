db9fc66692499690bf93d513dc1b6515ddd045c8
tmogoa <mogoa.tonny@gmail.com>
2022-08-15 13:10:22 +0300

    debug

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./App.css";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
             <div className="font-bold text-6xl text-blue-500 font-heading bg-white p-6 rounded">
                 Tony Mogoa
             </div>
+            <span className="text-xs text-white font-bold uppercase">
+                <Link to="/about">About me</Link>
+            </span>
         </div>
     );
 }
